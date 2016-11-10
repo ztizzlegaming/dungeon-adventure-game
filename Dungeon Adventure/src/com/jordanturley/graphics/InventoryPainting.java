3194c536722fe686ed9a13c052a2b180a91e3816
Jordan Turley <ztizzlegaming@gmail.com>
2016-11-10 16:44:57 -0500

    Paint the player's items in their inventory to the screen

--- Dungeon Adventure/src/com/jordanturley/graphics/InventoryPainting.java
+++ Dungeon Adventure/src/com/jordanturley/graphics/InventoryPainting.java
@@ -1,6 +1,6 @@
 package com.jordanturley.graphics;
 
-import java.awt.Color;
+import java.awt.Font;
 import java.awt.Graphics;
 import java.util.List;
 
@@ -10,11 +10,17 @@ import javax.swing.JComponent;
 import com.jordanturley.game.Player;
 import com.jordanturley.item.Item;
 
+/**
+ * The <code>InventoryPainting</code> class extends from JComponent and overrides the paint method, to
+ * paint the items in the player's inventory, their active weapon and armor, and health. 
+ * 
+ * @author Jordan Turley
+ */
 public class InventoryPainting extends JComponent {
 	private static final long serialVersionUID = 1L;
 	
-	private static final int NUM_ITEMS_WIDTH = 5;
-	private static final int NUM_ITEMS_HEIGHT = 3;
+	public static final int NUM_ITEMS_WIDTH = 5;
+	public static final int NUM_ITEMS_HEIGHT = 3;
 	
 	private Player player;
 
@@ -26,11 +32,10 @@ public class InventoryPainting extends JComponent {
 	
 	@Override
 	public void paint(Graphics g) {
+		//Calculate the size of the image when it is shown
 		int itemSize = getWidth() / NUM_ITEMS_WIDTH;
 		
-		Color[] colors = {Color.GREEN, Color.BLUE, Color.RED, Color.GRAY, Color.BLACK};
-		Color randColor = colors[(int) (Math.random() * colors.length)];
-		
+		//Draw the vertical and horizontal lines for their inventory
 		for (int y = 1; y < NUM_ITEMS_HEIGHT + 1; y++) {
 			g.drawLine(0, y * itemSize, getWidth(), y * itemSize);
 		}
@@ -49,5 +54,22 @@ public class InventoryPainting extends JComponent {
 			
 			g.drawImage(itemImageIcon.getImage(), x * itemSize, y * itemSize, itemSize, itemSize, null);
 		}
+		
+		//Draw the active weapon
+		Item activeWeapon = player.getActiveWeapon();
+		ImageIcon activeWeaponImage = activeWeapon.getImage();
+		g.drawImage(activeWeaponImage.getImage(), 0, NUM_ITEMS_HEIGHT * itemSize, itemSize, itemSize, null);
+		
+		//Draw the active armor
+		Item activeArmor = player.getActiveArmor();
+		ImageIcon activeArmorImage = activeArmor.getImage();
+		g.drawImage(activeArmorImage.getImage(), itemSize, NUM_ITEMS_HEIGHT * itemSize, itemSize, itemSize, null);
+		
+		//Draw the player's health
+		g.setFont(new Font(g.getFont().getFontName(), Font.PLAIN, 22));
+		String weightStr = "Weight:" + player.getInventoryWeight() + "/" + player.getMaxInventoryWeight();
+		g.drawString(weightStr, itemSize * 2 + 10, NUM_ITEMS_HEIGHT * itemSize + itemSize / 2 + Window.TEXT_OFFSET);
+		
+		//TODO Add XP, and show the player's level and current xp below their health
 	}
 }
